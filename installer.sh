83d77ccdc702c02df899b538c1a763c8a49a57fd
Bleyom <57446911+Bleyom@users.noreply.github.com>
2022-02-18 01:21:03 +0100

    add more more bloat

--- installer.sh
+++ installer.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+MAGENTA='\033[1;35m'
+GREY='\033[1;37m'
+ORANGE='\033[33m'
+
 os="$(grep -m1 "NAME=" </etc/os-release | cut -d '"' -f 2)"
 server="$(echo "$XDG_SESSION_TYPE")"
 help() {
-    echo "usage: $0 Install [-i] Help menu [-h]" >&2
+    echo -ne "${BLUE}usage: $0 Install [-i] Help menu [-h]${NC}\n" >&2
     exit 1
 }
 
 welcome-msg() {
-    echo -ne "┌─> Github: https://github.com/Bleyom/ \n"
-    echo -ne "└────────> The script only works for Arch Linux, Void Linux (Working in Debian and another distros support)\n"
+    echo -ne "${GREEN}┌─> Github: ${MAGENTA}https://github.com/Bleyom/ ${NC}\n"
+    echo -ne "${GREEN}└────────>${NC} The script only works for ${BLUE}Arch Linux${NC}, ${ORANGE}Void Linux${NC} ${GREY}(Working in Debian and another distros support)${NC}\n"
 }
 
 check_server() {
     if [ "$server" == x11 ]; then
-        echo -ne "[*] You are using X11"
+        echo -ne "${RED}[*] You are using X11${NC}"
         if [ "$os" == "Arch Linux" ]; then
             paru -Sy eww --noconfirm
         elif [ "$os" == "void" ]; then
             cp -r eww-template/eww srcpkgs/eww
         fi
     else
-        echo -ne "[*] You are using Wayland or idk"
+        echo -ne "${GREEN}[*] You are using Wayland or idk${NC}"
         if [ "$os" == "Arch Linux" ]; then
             paru -Sy eww-wayland-git --noconfirm
         elif [ "$os" == "void" ]; then
@@ -31,21 +39,21 @@ check_server() {
 
 detect_os() {
     if [ "$os" == "void" ]; then
-        echo -ne "[*] Welcome to BiseBis (My dotfiles) Installer\n"
-        echo -ne "Os used is Void Linux\n"
-        echo -ne "Proceding with Void Linux installation\n"
+        echo -ne "${GREEN}[*] Welcome to BiseBis (My dotfiles) Installer\n"
+        echo -ne "${RED}Os used is ${RED}Void Linux${NC}\n"
+        echo -ne "${GREEN}Proceding with Void Linux installation${NC}\n"
         install-void
     elif [ "$os" == "Arch Linux" ]; then
-        echo -ne "[*] Welcome to BiseBis (My dotfiles) Installer\n"
-        echo -ne "Os used is Arch Linux\n"
-        echo -ne "Proceding with Arch Linux installation\n"
+        echo -ne "${GREEN}[*] Welcome to BiseBis (My dotfiles) Installer\n"
+        echo -ne "${RED}Os used is ${RED}Arch Linux${NC}\n"
+        echo -ne "${GREEN}Proceding with Arch Linux installation${NC}\n"
         install-arch
     elif [ "$os" == "Debian" ]; then
         # working on this //
-        echo -ne "[*] Welcome to BiseBis (My dotfiles) Installer\n"
-        echo -ne "Os used is Debian Linux\n"
-        echo -ne "Proceding with Debian installation\n"
-        echo -ne "Working in that ...\n"
+        echo -ne "${GREEN}[*] Welcome to BiseBis (My dotfiles) Installer\n"
+        echo -ne "${RED}Os used is ${RED}Debian${NC}\n"
+        echo -ne "${GREEN}Proceding with Debian installation${NC}\n"
+        echo -ne "${RED}Working in that ...${NC}\n"
     else
         echo -ne "[*] Welcome to BiseBis (My dotfiles) Installer\n"
         echo -ne "It's not a supported distro proceding with default installation (Packages doesn't get installed) \n"
@@ -55,12 +63,12 @@ detect_os() {
 
 install-paru() {
     if pacman -Qs git base-devel fakeroot >/dev/null; then
-        echo -ne "[*] Git and another dependencies are installed, cloning and installing AUR Helper(paru)\n"
+        echo -ne "[*] Git and another dependencies are installed, cloning and installing AUR Helper (paru)\n"
         git clone https://aur.archlinux.org/paru-bin.git
         cd paru-bin || exit
         makepkg -si
     else
-        echo -ne "[*] Git and another depedencies are not installed, Installing dependencies and cloning and installing AUR Helper(paru)\n"
+        echo -ne "[*] Git and another depedencies are not installed, Installing dependencies and cloning and installing AUR Helper (paru)\n"
         sudo pacman -S git base-devel fakeroot --noconfirm
         git clone https://aur.archlinux.org/paru-bin.git
         cd paru-bin || exit
@@ -79,9 +87,9 @@ install-common() {
 }
 
 install-void() {
-    echo -ne "[*] Installing Dependencies\n"
+    echo -ne "${GREEN}[*] Installing Dependencies${NC}\n"
     sudo xbps-install xtools git polybar sxhkd bspwm kitty rofi picom dunst neofetch nerd-fonts-ttf
-    echo -ne "[*] Cloning eww template repositorie\n"
+    echo -ne "${GREEN}[*] Cloning eww template repositorie${NC}\n"
     git clone https://github.com/void-linux/void-packages
     cd void-packages || exit
     git clone https://github.com/monke0192/eww-template
@@ -93,7 +101,7 @@ install-void() {
 
 install-arch() {
     install-paru
-    echo -ne "[*] Installing dependencies\n"
+    echo -ne "${GREEN}[*] Installing dependencies${NC}\n"
     paru -Sy polybar sxhkd bspwm kitty rofi picom dunst neofetch nerd-fonts-complete
     check_server
     copy-config
@@ -123,7 +131,6 @@ done
 trap ctrl_c INT
 
 function ctrl_c() {
-        echo "[*] Ciao ^^"
+        echo "${RED}[*] Ciao ^^${NC}"
 }
 
-help
